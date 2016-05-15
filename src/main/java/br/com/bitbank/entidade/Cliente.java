52b5e79f4641a4e8ad6e05e73d7c629928a185f1
acgustavo <dj_gustavo_@hotmail.com>
2016-05-15 19:55:07 -0300

    v 0.0.1

---
+++ src/main/java/br/com/bitbank/entidade/Cliente.java
@@ -0,0 +1,76 @@
+package br.com.bitbank.entidade;
+
+import java.util.Calendar;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String nome;
+	private String email;
+	private String endereco;
+	@Temporal(TemporalType.DATE)
+	@Column(name = "data_nascimento")
+	private Calendar dataNascimento;
+	private String cpf;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public Calendar getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Calendar dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+}
---
+++ src/main/java/br/com/gu/jdbc/JPAUtil.java
@@ -0,0 +1,14 @@
+package br.com.gu.jdbc;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JPAUtil {
+	private static EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("BitbankPU");
+
+	public EntityManager getEntityManager() {
+		return entityManagerFactory.createEntityManager();
+	}
+
+}
